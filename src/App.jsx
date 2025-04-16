b6a3015fb3c363516f19ff34b4d8ab402d7ea3e8
misterEugene <mistersmister66@gmail.com>
2025-04-16 13:56:07 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 function App() {
-  const [todo, setTodo] = useState([])
-  cosnt [newTodo, setNewTodo] = useState('')
+
+  const [fields, setFields] = useState([1,2,3,4,5,6,7,8,9])
+  
+
+  console.log(fields.map(item => <div className="space_gaming"></div>));
+        
+  
 
   return (
     <>
-      <input  type="text"/>
-      <button>Добавить заметку</button>
+    <div className="root">
+      <div className='play_board'>
+        {fields.map(item => <div data-field-id={item} key={item} className="space_gaming"></div>)}
+      </div>
+    </div>
+    
     </>
   )
 }
